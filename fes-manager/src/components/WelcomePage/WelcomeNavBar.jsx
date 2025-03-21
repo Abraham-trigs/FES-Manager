c1d424caeddbf584e3ceaf7a08e318ea383eb4e2
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-03-21 01:08:13 +0000

    WelcomeNavBar.jsx positioned and styled

--- fes-manager/src/components/WelcomePage/WelcomeNavBar.jsx
+++ fes-manager/src/components/WelcomePage/WelcomeNavBar.jsx
@@ -4,20 +4,24 @@ const WelcomeNavBar = () => {
   return (
     <>
       <div className="relative">
-        {/* Logo positioned at the top left */}
-        <div className="absolute top-2 left-4">
+        
+        {/* Logo & Text*/}
+        <div className="absolute top-2 left-4 flex items-center space-x-4">
           <img src="/images/logo.png" 
-            alt="logo" 
-            className="w-[10%] h-auto" />
-            
-        <p>How it Works</p>
+               alt="logo" 
+               className="w-[10%] h-auto" />
+
+          <p className="text-cyanNeon text-lg font-semibold">
+            How it Works
+          </p>
         </div>
 
         {/* Header background */}
         <div>
-          <div className="w-full h-16 bg-darkGreen mx-auto"></div>
+          <div className="w-full h-[70px] bg-darkGreen mx-auto"></div>
           <div className="w-full h-[5px] bg-greenNeon mx-auto m-[3]"></div>
         </div>
+        
       </div>
     </>
   );
