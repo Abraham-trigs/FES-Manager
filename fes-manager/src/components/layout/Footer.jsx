98e891b4bc2b30b18f6f4d7cc9a310a4036e5ac2
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-07-29 19:34:43 +0000

    Readme writen in more detailed but still needs update. Footer message changed

--- fes-manager/src/components/layout/Footer.jsx
+++ fes-manager/src/components/layout/Footer.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     // Footer container positioned at the bottom of the screen
     <div className="fixed bottom-0 left-0 w-full z-50">
-      
       {/* Footer background and content */}
       <div>
         {/* Thin green line at the top of the footer */}
@@ -13,7 +12,8 @@ const Footer = () => {
         {/* Main footer section with background and centered text */}
         <div className="w-full h-[30px] bg-darkGreen dark:bg-dark flex justify-center items-center">
           <p className="text-cyanNeon dark:text-clear text-[0.5rem] ">
-            <span className='text-white '>powered by</span>  LEGEND FOUNDATION &copy; 2025
+            <span className="text-white">FES-Manager + TymUp:</span> Funding
+            hope. Building futures. Together.
           </p>
         </div>
       </div>
